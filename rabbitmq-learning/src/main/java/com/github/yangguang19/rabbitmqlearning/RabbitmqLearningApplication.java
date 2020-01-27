f69417ac3669d0430cb896fd7dc5ac801021f4dd
YangGuang19 <2196927727@qq.com>
2020-01-27 15:53:17 +0800

    更新Spring与Rabbitmq整合

--- rabbitmq-learning/src/main/java/com/github/yangguang19/rabbitmqlearning/RabbitmqLearningApplication.java
+++ rabbitmq-learning/src/main/java/com/github/yangguang19/rabbitmqlearning/RabbitmqLearningApplication.java
@@ -1,8 +1,10 @@
 package com.github.yangguang19.rabbitmqlearning;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class RabbitmqLearningApplication
 {
--- rabbitmq-learning/src/main/java/com/github/yangguang19/rabbitmqlearning/spring_rabbitmq/config/RabbitMQConfig.java
+++ rabbitmq-learning/src/main/java/com/github/yangguang19/rabbitmqlearning/spring_rabbitmq/config/RabbitMQConfig.java
@@ -2,9 +2,11 @@ package com.github.yangguang19.rabbitmqlearning.spring_rabbitmq.config;
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -17,10 +19,35 @@ public class RabbitMQConfig
 {
 
     @Bean
-    public RabbitAdmin rabbitAdmin(@Qualifier("rabbitConnectionFactory") ConnectionFactory connectionFactory)
+    public RabbitAdmin rabbitAdmin(@Qualifier("tRabbitTemplate")RabbitTemplate rabbitTemplate)
     {
-      RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-      rabbitAdmin.setAutoStartup(true);
-      return rabbitAdmin;
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(rabbitTemplate);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+    @Bean("tRabbitTemplate")
+    public RabbitTemplate rabbitTemplate( @Qualifier("rabbitConnectionFactory") ConnectionFactory connectionFactory)
+    {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) ->
+        {
+            System.out.println("correlation : " + correlationData);
+            if (!ack) {
+                System.err.println(cause);
+            }
+        };
+        RabbitTemplate.ReturnCallback returnCallback = (message, replyCode, replyText, exchange, routingKey) ->
+        {
+            System.out.println(message);
+            System.out.println(replyCode);
+            System.out.println(replyText);
+            System.out.println(exchange);
+            System.out.println(routingKey);
+        };
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnCallback(returnCallback);
+        return rabbitTemplate;
     }
 }
