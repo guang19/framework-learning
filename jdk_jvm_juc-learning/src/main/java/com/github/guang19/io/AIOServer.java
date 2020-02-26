bcb937c3d2b7511b122b8eefbc6efdc7f1051066
YangGuang19 <2196927727@qq.com>
2020-02-26 21:55:52 +0800

    fix:修改jvm若干错误

--- jdk_jvm_juc-learning/src/main/java/com/github/guang19/io/AIOServer.java
+++ jdk_jvm_juc-learning/src/main/java/com/github/guang19/io/AIOServer.java
@@ -2,9 +2,8 @@ package com.github.guang19.io;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.AsynchronousChannel;
-import java.nio.channels.AsynchronousFileChannel;
-import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.CharBuffer;
+import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -20,9 +19,32 @@ public class AIOServer
 
     public static void main(String[] args) throws Exception
     {
-        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open();
-        server.bind(new InetSocketAddress("127.0.0.1",8080));
-        server.accept();
+        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open().
+                bind(new InetSocketAddress("127.0.0.1",8080));
+        while (true)
+        {
+            //阻塞至有客户端连接
+            AsynchronousSocketChannel client  = server.accept().get();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            byteBuffer.clear();
+            while (true)
+            {
+                //设定5秒钟,如果还没有读取完成客户端的数据,那么直接break
+                Integer result = client.read(byteBuffer).get(5L, TimeUnit.SECONDS);
+                //数据读取完成
+                if(result == -1)
+                {
+                    byteBuffer.flip();
+                    System.out.println("client data : " + StandardCharsets.UTF_8.decode(byteBuffer));
+                    byteBuffer.clear();
+                    try
+                    {
+                        client.close();
+                    }catch (Exception e){}
+                    break;
+                }
+            }
+        }
     }
 
 }
