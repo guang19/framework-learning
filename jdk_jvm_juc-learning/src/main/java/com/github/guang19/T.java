0f1dfc0e969f5acd272be7ec189366a6ec3cd8b7
YangGuang19 <2196927727@qq.com>
2020-03-02 22:01:22 +0800

    fix:GC算法错误之处

---
+++ jdk_jvm_juc-learning/src/main/java/com/github/guang19/T.java
@@ -0,0 +1,16 @@
+package com.github.guang19;
+
+/**
+ * @author yangguang
+ * @date 2020/3/2
+ * @description <p></p>
+ */
+public class T
+{
+    private final String a1 = "a";
+    private static final String a2 = "a";
+    public static void main(String[] args)
+    {
+
+    }
+}
