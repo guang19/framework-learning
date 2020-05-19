ed08cad9f296cb23d13ceb4a0d551bcb67f5bbdd
guang19 <2196927727@qq.com>
2020-05-19 23:41:59 +0800

    增加:单例模式

---
+++ design-pattern/src/main/java/com/github/guang19/designpattern/singleton/SingletonTest.java
@@ -0,0 +1,15 @@
+package com.github.guang19.designpattern.singleton;
+
+/**
+ * @author guang19
+ * @date 2020/5/19
+ * @description
+ * @since 1.0.0
+ */
+public class SingletonTest
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
